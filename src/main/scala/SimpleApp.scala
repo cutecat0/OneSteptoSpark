42617ba9d284704368f07ce84343c943bd601206
Simba <37787934+cutecat0@users.noreply.github.com>
2022-04-24 16:44:29 +0800

    add run your scala through jar

---
+++ src/main/scala/SimpleApp.scala
@@ -0,0 +1,25 @@
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+
+object SimpleApp {
+  def main(args: Array[String]) {
+    val logFile = "/Users/xxx/Downloads/spark-3.2.1-bin-hadoop3.2/bin/README.md"
+    val conf = new SparkConf().setAppName("Simple Application")
+    val sc = new SparkContext(conf)
+    val logData = sc.textFile(logFile, 2).cache()
+    val numAs = logData.filter(line => line.contains("a")).count()
+    val numBs = logData.filter(line => line.contains("b")).count()
+    println("Lines with a: %s \n Lines with b: %s".format(numAs, numBs))
+  }
+}
+
+// Use spark-submit to run your application
+//$ YOUR_SPARK_HOME/bin/spark-submit \
+//--class "SimpleApp" \
+//--master local[4] \ here I run local without [4]
+//  target/scala-2.12/simple-project_2.12-1.0.jar
+//  ...
+//Lines with a: 14
+// Lines with b: 7
+
